08b6e5a17a4510bc007749c7c512442723e6361c
Aditya <adityabhadauria63@gmail.com>
2026-07-09 02:26:05 +0530

    Allow Vercel frontend in CORS

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,7 @@ const corsOptions =
     // ],
     origin: (origin, callback) => {
         if (!origin) return callback(null, true);
-        if (['http://localhost:5173', 'http://localhost:5174'].includes(origin)) {
+        if (['http://localhost:5173', 'http://localhost:5174','https://interview-buddy-tau.vercel.app'].includes(origin)) {
             return callback(null, true);
         }
         else if (process.env.NODE_ENV === "production") {
@@ -55,7 +55,8 @@ const io = new Server(server, {
     cors: {
         origin: [
             "http://localhost:5173",
-            "http://localhost:5174"
+            "http://localhost:5174",
+            "https://interview-buddy-tau.vercel.app"
         ],
         // origin: allowedOrigins,
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
